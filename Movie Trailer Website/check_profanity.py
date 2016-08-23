e4eef6aadeefa16a6dc5d7f24d993df8ed21c079
DmitryGrechko <GrechkoDmytro@Grechkos-MacBook-Pro-3.local>
2016-08-22 21:09:32 -0400

    Style Edit

--- Movie Trailer Website/check_profanity.py
+++ Movie Trailer Website/check_profanity.py
@@ -1,16 +1,19 @@
 import urllib
 
+
 def read_text():
-    quotes = open("/Users/GrechkoDmytro/Desktop/Udacity/Full-Stack Web Development NanoDegree/Movie Trailer Website/movie_quotes.txt")
+    quotes = open(
+        "/Users/GrechkoDmytro/Desktop/Udacity/Full-Stack Web Development NanoDegree/Movie Trailer Website/movie_quotes.txt")
     contents = quotes.read()
-    #print contents
+    # print contents
     quotes.close()
     check_profanity(contents)
+
 
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=shot" + text_to_check)
     output = connection.read()
-    #print output
+    # print output
     connection.close()
     if "true" in output:
         print "Profanity Alert!!"
@@ -18,5 +21,6 @@ def check_profanity(text_to_check):
         print "This document has no curse words!"
     else:
         print "Couldn't scan the document properly"
+
 
 read_text()
