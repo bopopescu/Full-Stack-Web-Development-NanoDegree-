f0ad4a99364f0ea964ea0d038b24722da2791d65
Your Name <GrechkoDmytro@Grechkos-MacBook-Pro-3.local>
2017-02-16 14:04:52 -0500

    Started tournaments db, + registr/count/deletiong of players/matches

--- Lesson 4 _ Intro to Relational Databases/FSND-Virtual-Machine/vagrant/tournament/tournament.sql
+++ Lesson 4 _ Intro to Relational Databases/FSND-Virtual-Machine/vagrant/tournament/tournament.sql
@@ -7,3 +7,13 @@
 -- these lines here.
 
 
+CREATE DATABASE tournament;
+\c tournament;
+
+DROP tournament IF EXISTS;
+
+CREATE TABLE IF NOT EXISTS players(id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE IF NOT EXISTS matches (id SERIAL PRIMARY KEY,
+    player1 SERIAL references players(id),
+    player2 SERIAL references players(id)
+);
