46f33254156af7eba66fd90d7ee2c82c8a3df378
Your Name <GrechkoDmytro@Grechkos-MacBook-Pro-3.local>
2017-04-06 19:52:14 -0400

    Added Templates & Routing to Final Project

--- Lesson 4 : 5 _ Intro to Relational Databases : Item Catalog/FSND-Virtual-Machine/vagrant/webserver/project.py
+++ Lesson 4 : 5 _ Intro to Relational Databases : Item Catalog/FSND-Virtual-Machine/vagrant/webserver/project.py
@@ -27,16 +27,30 @@ def menuItemJSON(restaurant_id, menu_id):
 
 
 @app.route('/')
+@app.route('/restaurants')
+def restaurantsList(restaurant_id):
+
+
+@app.route('/restaurant/new')
+def restaurantNew(restaurant_id):
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit')
+def restaurantEdit(restaurant_id):
+
+
+@app.route('/restaurant/<int:restaurant_id>/delete')
+def restaurantDelete(restaurant_id):
+
+
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
 
-# Task 1: Create route for newMenuItem function here
 
-
-@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
         newItem = MenuItem(
@@ -48,10 +62,8 @@ def newMenuItem(restaurant_id):
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
-# Task 2: Create route for editMenuItem function here
 
-
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
     editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
     if request.method == 'POST':
@@ -64,10 +76,8 @@ def editMenuItem(restaurant_id, menu_id):
     else:
         return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
-# Task 3: Create a route for deleteMenuItem function here
 
-
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
     deletedItem = session.query(MenuItem).filter_by(id=menu_id).one()
     if request.method == 'POST':
